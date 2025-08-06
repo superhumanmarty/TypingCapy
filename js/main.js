5f51428e5f2bf17c34f819701b384060fdcc599b
ballerdesignergod <hendricks.marty88@gmail.com>
2025-08-06 13:43:15 -0500

    test change

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // main.js
-
+// hi this is a change
 import { updateHighlight } from './highlight.js';
 import { calculateWPM, calculateAccuracy, getCurrentWord } from './utils.js';
 import { updateHide } from './hide.js';
